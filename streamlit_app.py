5b3faabbef14957cd2039326bbbfa5843aa3170d
Diego Esteve <64361674+ogeid6@users.noreply.github.com>
2024-02-14 13:58:50 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import urllib.error
 # menu  
 streamlit.title("Breakfast Favorites")
 streamlit.header('Breakfast Menu')
-streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(" 🥣 Omega 3 & Blueberry Oatmeal")
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avocado Toast')
