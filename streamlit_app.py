a13fe4533e29fb0482330fff7230b7084aac195d
Diego Esteve <64361674+ogeid6@users.noreply.github.com>
2024-02-14 10:56:39 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,8 +31,7 @@ try:
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
   fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
   streamlit.dataframe(fruityvice_normalized)
-except URLError as e:
-  streamlit.error()
+
 
 
 # query of snowflake fruit list
